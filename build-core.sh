7c6d412661ba742716b03aa4ccb79b9d3010b39b
nikso-itu <nikso@itu.dk>
2024-03-04 12:34:06 +0100

    added command for unzipping image

--- build-core.sh
+++ build-core.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 . sources/poky/oe-init-build-env
 bitbake core-image-base
+bzip2 -d -f tmp/deploy/images/raspberrypi3-64/core-image-base-raspberrypi3-64.wic.bz2
 
 # BBLAYERS ?= " \
 #   ${TOPDIR}/../sources/poky/meta \
